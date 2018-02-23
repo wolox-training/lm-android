4040f079bce327dfe9e2c4e7a3a2cbb8c3928230
Lucas Miotti <lucas.miotti@wolox.com.ar>
2018-02-23 12:45:05 -0300

    Wolox icon added, email and password edit texts added and log in and sign up buttons added

--- app/src/main/java/ar/com/wolox/android/LogIn/ui/login/LogInFragment.java
+++ app/src/main/java/ar/com/wolox/android/LogIn/ui/login/LogInFragment.java
@@ -1,6 +1,8 @@
 package ar.com.wolox.android.LogIn.ui.login;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 import ar.com.wolox.android.R;
@@ -10,6 +12,8 @@ import butterknife.BindView;
 import butterknife.OnClick;
 
 public class LogInFragment extends WolmoFragment<LogInPresenter> implements LogInView {
+
+    @BindView(R.id.fragment_login_terms) TextView mTermsTxt;
 
     // Fragments default constructors shouldn't be overridden, always prefer this method instead
     public static LogInFragment newInstance() {
@@ -22,6 +26,12 @@ public class LogInFragment extends WolmoFragment<LogInPresenter> implements LogI
     @Override
     public int layout() {
         return R.layout.fragment_login;
+    }
+
+    @Override
+    public void setUi(View v) {
+        super.setUi(v);
+        mTermsTxt.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
