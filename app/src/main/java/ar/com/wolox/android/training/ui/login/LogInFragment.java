f2c599294272d57ec831e5b22e37f0d3cbf1bba7
Lucas Miotti <lucas.miotti@wolox.com.ar>
2018-03-06 16:15:58 -0300

    Set transformation method for password edit text in LogInFragment. RootActivity has been changed to load key and compare it with server information.

--- app/src/main/java/ar/com/wolox/android/training/ui/login/LogInFragment.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/login/LogInFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -48,10 +49,7 @@ public class LogInFragment extends WolmoFragment<LogInPresenter> implements LogI
                 if (mPasswordTxt.length() == 0)
                     mPasswordTxt.setError("Please complete with your password");
             }
-
     }
-
-
 
     @OnClick(R.id.fragment_login_signup_button)
         public void SignUpCall(){
@@ -68,6 +66,7 @@ public class LogInFragment extends WolmoFragment<LogInPresenter> implements LogI
     public void setUi(View v) {
         super.setUi(v);
         mTermsTxt.setMovementMethod(LinkMovementMethod.getInstance());
+        mPasswordTxt.setTransformationMethod(PasswordTransformationMethod.getInstance() );
     }
 
     @Override
@@ -88,8 +87,7 @@ public class LogInFragment extends WolmoFragment<LogInPresenter> implements LogI
         editor.putString(USER, mEmailTxt.getText().toString());
         editor.commit();
         Intent homeIntent = new Intent(getActivity(), HomeActivity.class);
-        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(homeIntent);
     }
 
