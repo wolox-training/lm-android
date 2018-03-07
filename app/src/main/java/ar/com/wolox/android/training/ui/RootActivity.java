8ec23e446db518072afb0cf5ec6fa4aaea7a6c0f
Lucas Miotti <lucas.miotti@wolox.com.ar>
2018-03-07 12:08:47 -0300

    Unnecesary libraries erased

--- app/src/main/java/ar/com/wolox/android/training/ui/RootActivity.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/RootActivity.java
@@ -4,8 +4,6 @@ package ar.com.wolox.android.training.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import java.util.List;
 
@@ -13,13 +11,10 @@ import ar.com.wolox.android.R;
 import ar.com.wolox.android.training.TrainingApplication;
 import ar.com.wolox.android.training.ui.home.HomeActivity;
 import ar.com.wolox.android.training.ui.login.LogInActivity;
-import ar.com.wolox.android.training.ui.login.LogInFragment;
-import ar.com.wolox.android.training.ui.login.LogInView;
 import ar.com.wolox.android.training.ui.network.entities.UserResponse;
 import ar.com.wolox.android.training.ui.network.services.LogInService;
 import ar.com.wolox.wolmo.core.activity.WolmoActivity;
 import ar.com.wolox.wolmo.networking.retrofit.RetrofitServices;
-import butterknife.BindView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
