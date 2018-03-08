b421600a0cef6c0ababcd76f04466f7b35ff7ce3
Lucas Miotti <lucas.miotti@wolox.com.ar>
2018-03-08 15:46:01 -0300

    News and Profile imageview change color when selected

--- app/src/main/java/ar/com/wolox/android/training/ui/home/HomeFragment.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/home/HomeFragment.java
@@ -1,18 +1,54 @@
 package ar.com.wolox.android.training.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ar.com.wolox.android.R;
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class HomeFragment extends WolmoFragment<HomePresenter> implements HomeView {
+
+    @BindView(R.id.home_news_tab) LinearLayout mNewsll;
+    @BindView(R.id.home_profile_tab) LinearLayout mProfilell;
+    @BindView(R.id.home_news_icon_on) ImageView mNewsOnimg;
+    @BindView(R.id.home_news_icon_off) ImageView mNewsOffimg;
+    @BindView(R.id.home_profile_icon_on) ImageView mProfileOnimg;
+    @BindView(R.id.home_profile_icon_off) ImageView mProfileOffimg;
+    @BindView(R.id.home_news_text_view) TextView mNewstxt;
+    @BindView(R.id.home_profile_text_view) TextView mProfiletxt;
 
     public static HomeFragment newInstance() {
         Bundle args = new Bundle();
         HomeFragment fragment = new HomeFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @OnClick(R.id.home_news_tab)
+    public void newsSet(){
+        mNewsOnimg.setVisibility(View.VISIBLE);
+        mNewsOffimg.setVisibility(View.GONE);
+        mProfileOnimg.setVisibility(View.GONE);
+        mProfileOffimg.setVisibility(View.VISIBLE);
+        mNewstxt.setTextColor(0x8DD35F);
+        mProfiletxt.setTextColor(0x000000);
+    }
+
+    @OnClick(R.id.home_profile_tab)
+    public void profileSet(){
+        mNewsOnimg.setVisibility(View.GONE);
+        mNewsOffimg.setVisibility(View.VISIBLE);
+        mProfileOnimg.setVisibility(View.VISIBLE);
+        mProfileOffimg.setVisibility(View.GONE);
+        mNewstxt.setTextColor(0x000000);
+        mProfiletxt.setTextColor(0x8DD35F);
     }
 
     @Override
