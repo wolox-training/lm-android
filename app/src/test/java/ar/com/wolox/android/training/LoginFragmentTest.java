89ec3fc0ea9d917f2815efa4740b9359615c61db
Lucas Miotti <lucas.miotti@wolox.com.ar>
2018-03-08 09:46:01 -0300

    jUnit dependency incorporated

---
+++ app/src/test/java/ar/com/wolox/android/training/LoginFragmentTest.java
@@ -0,0 +1,8 @@
+package ar.com.wolox.android.training;
+
+/**
+ * Created by lucasmiotti on 08/03/18.
+ */
+
+public class LoginFragmentTest {
+}
---
+++ app/src/test/java/ar/com/wolox/android/training/ui/login/LogInFragmentTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by lucasmiotti on 07/03/18.
+ */
+public class LogInFragmentTest {
+
+}
